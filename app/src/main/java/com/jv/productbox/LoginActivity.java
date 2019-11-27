3baa2c75ba844735a452b41207f14a42374c22be
javyho <javyho@163.com>
2019-11-27 16:42:15 +0800

    login/register

---
+++ app/src/main/java/com/jv/productbox/LoginActivity.java
@@ -0,0 +1,74 @@
+package com.jv.productbox;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.fragment.app.FragmentActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends FragmentActivity {
+
+    @BindView(R.id.et_acc)
+    EditText etAccount;
+    @BindView(R.id.et_psw)
+    EditText etPassword;
+    @BindView(R.id.tv_register)
+    TextView tvRegister;
+    @BindView(R.id.tv_forgetPsw)
+    TextView tvForgetPsw;
+    @BindView(R.id.btn_login)
+    Button btLogin;
+
+    String account, password;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        tvRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                LoginActivity.this.startActivity(intent);
+            }
+        });
+        tvForgetPsw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                account = etAccount.getText().toString().trim();
+                password = etPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(account) || TextUtils.isEmpty(password)) {
+                    Toast.makeText(LoginActivity.this, "账号或密码为空，请检查！", Toast.LENGTH_SHORT).show();
+                } else {
+                    if ("admin".equals(account) && "admin".equals(password)) {
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        LoginActivity.this.startActivity(intent);
+
+                        LoginActivity.this.finish();
+                    } else {
+                        Toast.makeText(LoginActivity.this, "账号或密码不正确，请检查！", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/jv/productbox/RegisterActivity.java
@@ -0,0 +1,53 @@
+package com.jv.productbox;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @BindView(R.id.et_account)
+    EditText etAccount;
+    @BindView(R.id.et_psw)
+    EditText etPsw;
+    @BindView(R.id.btn_save)
+    Button btnSave;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        ButterKnife.bind(this);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String account = etAccount.getText().toString().trim();
+                String password = etPsw.getText().toString().trim();
+
+                if (TextUtils.isEmpty(account) || TextUtils.isEmpty(password)) {
+                    Toast.makeText(RegisterActivity.this, "账号或密码为空，请检查！", Toast.LENGTH_SHORT).show();
+                } else {
+                    saveToServer(account, password);
+
+                    finish();
+                }
+            }
+        });
+    }
+
+    private void saveToServer(String account, String psw) {
+        //todo 2019-11-27 16:27:41
+        Toast.makeText(this, "存到服务端", Toast.LENGTH_SHORT).show();
+    }
+}
